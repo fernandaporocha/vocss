91bdaf81af3c0b0eb5b0ce4d28637f2be585f253
Fernanda Rocha <fernandaporocha@gmail.com>
2021-03-10 20:10:23 +0000

    Adding UserProfile to the admin

--- userProfile/admin.py
+++ userProfile/admin.py
@@ -1,6 +1,62 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
+from django.contrib.auth.admin import UserAdmin
 from .models import UserProfile
+from django.contrib.auth.models import User
+
+class UserProfileInline (admin.StackedInline):
+    model = UserProfile
+    max_num = 1
+    fieldsets = (
+        (None, {
+            'fields': ('nome', 'dob')
+        }),
+        (_('Contact information'), {
+            'fields': ('email', 'phone', 'mobile')
+        }),
+        (_('Addr'), {
+            'fields': ('address', 'city', 'state')
+        }),
+    )
+    verbose_name = _('profile')
+    verbose_name_plural = _('profile')
+
+class UserProfileAdmin(UserAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'first_name', 'last_name', 'password', 'is_active', 'groups')
+        }),
+    )
+    list_display=('dob', 'phone', 'mobile', 'email', 'address', 'city', 'state')
+    inlines = (UserProfileInline,)
+
+    def dob (self, instance):
+        return instance.profile.dob
+    dob.short_description = _("dob")
+
+    def phone (self, instance):
+        return instance.profile.phone
+    phone.short_description = _("phone")
+
+    def mobile (self, instance):
+        return instance.profile.mobile
+    mobile.short_description = _("mobile")
+
+    def address (self, instance):
+        return instance.profile.address
+    address.short_description = _("address")
+
+    def city (self, instance):
+        return instance.profile.city
+    city.short_description = _("city")
+
+    def state (self, instance):
+        return instance.profile.state
+    state.short_description = _("state")
+    
+
 
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.unregister (User)
+admin.site.register(User, UserProfileAdmin)
--- vocss/settings.py
+++ vocss/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'userProfile.apps.UserprofileConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userProfile.apps.UserprofileConfig',
 ]
 
 MIDDLEWARE = [
