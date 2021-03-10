e7050dfa9d1c04a7a2555485f8c058278aea13e8
Fernanda Rocha <fernandaporocha@gmail.com>
2021-03-10 20:09:01 +0000

    Changing some fields from UserProfile

--- userProfile/models.py
+++ userProfile/models.py
@@ -4,9 +4,9 @@ from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class UserProfile (models.Model):
-    user = models.ForeignKey('auth.User', verbose_name=_("user"), on_delete=models.CASCADE)
+    user = models.OneToOneField('auth.User', verbose_name=_("user"), on_delete=models.CASCADE)
     nome = models.CharField(_("name"), max_length=100, blank=False, null=False)
-    dob = models.DateField(_("dob"), auto_now=False, blank=True, null=False)
+    dob = models.DateField(_("dob"), auto_now=False, blank=True, null=True)
     phone = models.CharField(_("phone"), max_length=100, blank=True, null=False)
     mobile = models.CharField(_("mobile"), max_length=100, blank=True, null=False)
     email = models.CharField(_("email"), max_length=100, blank=True, null=False)
@@ -17,3 +17,6 @@ class UserProfile (models.Model):
     class Meta:
         verbose_name = _("user")
         verbose_name_plural = _("users")
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
