b738c5f7d96c451a0ab516469c48eed04afc6c92
Fernanda Rocha <fernandaporocha@gmail.com>
2021-03-18 18:39:35 +0000

    Adding Course module

--- vocssapp/admin.py
+++ vocssapp/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import Unity
+from .models import Unity, Course
 
 class UnityAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ('name', 'responsible', 'phone', 'email', 'address', 'city', 'state')
+            'fields': ('name', 'responsible', 'phone', 'email', 'address', 'city', 'state', 'is_active')
+        }),
+    )
+
+class CourseAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'is_active')
         }),
     )
 
 # Register your models here.
 admin.site.register(Unity, UnityAdmin)
+admin.site.register(Course, CourseAdmin)
--- vocssapp/models.py
+++ vocssapp/models.py
@@ -11,7 +11,16 @@ class Unity (models.Model):
     address = models.CharField(_("address"), max_length=100, blank=True, null=False)
     city = models.CharField(_("city"), max_length=50, blank=True, null=False)
     state = models.CharField(_("state"), max_length=50, blank=True, null=False)
+    is_active = models.BooleanField (_("is active?"), default=True)
 
     class Meta:
         verbose_name = _("unity")
         verbose_name_plural = _("unities")
+
+class Course (models.Model):
+    name = models.CharField(_("name"), max_length=100, blank=False, null=False)
+    is_active = models.BooleanField (_("is active?"), default=True)
+    
+    class Meta:
+        verbose_name = _("course")
+        verbose_name_plural = _("courses")
